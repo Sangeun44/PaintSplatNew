265900dea369012072352c6169f79648995f8efe
Sangeun44 <eunsang@sas.upenn.edu>
2019-09-30 23:31:01 -0400

    fixed

---
+++ PaintSplat-master/Assets/Scripts/BoardBehavior.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using Photon;
+using UnityEngine.UI;
+
+public class BoardBehavior : Photon.MonoBehaviour {
+    public Text scoreText;
+    public GameObject SplatterPrefab;
+    public GameObject imageTarget;
+
+    public Text winText;
+    public Text loseText;
+
+    public float timer;
+    public int score = 0;
+
+    private List<GameObject> splatters = new List<GameObject>();
+
+    private void Update () {
+        timer += Time.deltaTime;
+
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            imageTarget.SetActive(!imageTarget.activeSelf);
+        }
+
+        if (score >= 1000) {
+            winText.enabled = true;
+            timer = 0;
+        }
+        if (timer > 500000) {
+            loseText.enabled = true;
+        }
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+
+        var other = collision.collider.gameObject;
+        Vector3 hit_position = other.transform.position;
+        if (other.CompareTag("Ball"))
+        {
+            score += 100;
+            scoreText.text = score.ToString();
+
+            PhotonNetwork.Destroy(other);
+            Quaternion rot =  Quaternion.AngleAxis(Random.Range(0f, 360f), new Vector3(0, 0, 1)) ; //*transform.rotation;
+            var splatter = Instantiate(SplatterPrefab, hit_position, rot) as GameObject;
+
+            splatter.GetComponent<Renderer>().material.color = other.GetComponent<Renderer>().material.color;
+
+            splatters.Add(splatter);
+        }
+
+    }
+}
---
+++ PaintSplat-master/Assets/Scripts/MobileNetwork.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class MobileNetwork : Photon.PunBehaviour
+{
+    private string roomName;
+
+    private void OnGUI()
+    {
+        GUILayout.Label(PhotonNetwork.connectionStateDetailed.ToString());
+    }
+
+    // TODO-2.a: 
+    // Copy and paste the Start() and OnJoinedLobby() methods from MobileNetwork_Cube.cs
+    private void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings("0.1");
+        roomName = "testing";
+        Screen.orientation = ScreenOrientation.Portrait;
+    }
+
+    public override void OnJoinedLobby()
+    {
+        //PhotonNetwork.JoinRandomRoom();
+        PhotonNetwork.JoinRoom(roomName);
+        base.OnJoinedLobby();
+    }
+
+    public override void OnJoinedRoom()
+	{
+		GetComponent<MobileShooter>().Activate();
+		base.OnJoinedRoom ();
+    }
+
+
+}
